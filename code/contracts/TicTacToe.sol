95a49aa2d9804a47d7d6aa4c782873ade88d16ad
Vikrant Goyal <vikrantgoyal97@gmail.com>
2018-11-09 11:06:46 +0530

    almost done

--- code/contracts/TicTacToe.sol
+++ code/contracts/TicTacToe.sol
@@ -1,7 +1,7 @@
-pragma solidity ^0.4.2;
+pragma solidity ^0.4.24;
 
 contract TicTacToe {
-	string board = "000000000";
+	string public board = "000000000";
 	uint32 public gameField = uint32(0);
 	address public challenger;
 	address public opponent;
@@ -14,28 +14,88 @@ contract TicTacToe {
 	address public winner;
 	address public currentPlayer;
 	uint public timeAtLastTurn = now;
+	uint isopp = 0;
 
 	constructor(address _owner) public {
         challenger=msg.sender;
+        currentPlayer=challenger;
   	}
 
   	function getopponenet() public{
   		require(msg.sender!=challenger,"Challenger cannt be opponent");
-  		opponenet=msg.sender;
+  		opponent=msg.sender;
+  		isopp =1;
   	}
 
   	function playerMove (uint8 x, uint8 y) public{
   		require(winner==0);
-  		require(turncount<=9);
+  		require(turnCount<=9);
   		require(3*x+y<=8);
   		require(3*x+y>=0);
   		require(board[3*x+y]=="0");
-  		
+  		require(currentPlayer==msg.sender);
+  		require(isopp==1);
+  		string value;
   		if(msg.sender==challenger){
-  		board[3*x+y]="x";
+	  		board[3*x+y]="x";
+	  		value="x";
+	  		if(board[0]==value && board[1]==value && board[2]==value){
+	  			winner=1;
+	  		}
+	  		if(board[3]==value && board[4]==value && board[5]==value){
+	  			winner=1;
+	  		}
+	  		if(board[6]==value && board[7]==value && board[8]==value){
+	  			winner=1;
+	  		}
+	  		if(board[0]==value && board[3]==value && board[6]==value){
+	  			winner=1;
+	  		}
+	  		if(board[1]==value && board[4]==value && board[7]==value){
+	  			winner=1;
+	  		}
+	  		if(board[2]==value && board[5]==value && board[8]==value){
+	  			winner=1;
+	  		}
+	  		if(board[0]==value && board[4]==value && board[8]==value){
+	  			winner=1;
+	  		}
+	  		if(board[2]==value && board[4]==value && board[6]==value){
+	  			winner=1;
+	  		}
+	  		currentPlayer=opponent;
   		}
+  		
   		if(msg.sender==opponent){
-  		board[3*x+y]="O";
+	  		board[3*x+y]="O";
+	  		value="O";
+	  		if(board[0]==value && board[1]==value && board[2]==value){
+	  			winner=1;
+	  		}
+	  		if(board[3]==value && board[4]==value && board[5]==value){
+	  			winner=1;
+	  		}
+	  		if(board[6]==value && board[7]==value && board[8]==value){
+	  			winner=1;
+	  		}
+	  		if(board[0]==value && board[3]==value && board[6]==value){
+	  			winner=1;
+	  		}
+	  		if(board[1]==value && board[4]==value && board[7]==value){
+	  			winner=1;
+	  		}
+	  		if(board[2]==value && board[5]==value && board[8]==value){
+	  			winner=1;
+	  		}
+	  		if(board[0]==value && board[4]==value && board[8]==value){
+	  			winner=1;
+	  		}
+	  		if(board[2]==value && board[4]==value && board[6]==value){
+	  			winner=1;
+	  		}
+	  		currentPlayer=challenger;
   		}
+
   	}
+
 }
